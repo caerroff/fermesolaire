e69719499d241e8b145d578e14b230bb42092d78
caerroff <masin49@gmail.com>
2024-06-26 11:49:58 +0200

    feat(admin): add user CRUD

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,26 +3,67 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(
+        public UserPasswordHasherInterface $userPasswordHasher
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('username'),
+            TextField::new('password', 'Password')->hideOnIndex()
+                ->onlyOnForms()
+                ->setFormTypeOptions([
+                    'mapped' => false,
+                    'empty_data' => function ($entity) {
+                        return $entity->getPassword();
+                    },
+                    'hash_property_path' => 'password',
+                ])
+                ->setFormType(PasswordType::class),
+            ChoiceField::new('roles')->setChoices([
+                'User' => 'ROLE_USER',
+                'Admin' => 'ROLE_ADMIN',
+            ])->allowMultipleChoices(),
         ];
     }
-    */
+
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions->remove(Crud::PAGE_INDEX, Action::NEW)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->remove(Crud::PAGE_DETAIL, Action::DELETE)
+            ->remove(Crud::PAGE_DETAIL, Action::EDIT);
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $actions->add(Crud::PAGE_INDEX, Action::EDIT)
+                ->add(Crud::PAGE_INDEX, Action::NEW)
+                ->add(Crud::PAGE_INDEX, Action::DELETE)
+                ->add(Crud::PAGE_DETAIL, Action::DELETE)
+                ->add(Crud::PAGE_DETAIL, Action::NEW);
+        }
+        return $actions;
+    }
 }
