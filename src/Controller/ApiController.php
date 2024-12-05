90eab51368b0a764194c63eff2871d1d1b972a6a
Caerroff <56608891+caerroff@users.noreply.github.com>
2024-12-05 15:00:01 +0200

    10 enhance front (#25)
    * feat(front): enhance front
    
    * fix(build): add build

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,11 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
 {
+    private $mapbox_token;
+
+    public function __construct()
+    {
+        $this->mapbox_token = $_ENV['MAPBOX_TOKEN'];
+    }
+
     #[Route('/api/fetchParcelle/{lon}/{lat}', name: 'app_api_fetch_parcelle', options: ['expose' => true])]
     public function fetchParcelle(string $lon, string $lat): Response
     {
@@ -16,5 +24,11 @@ class ApiController extends AbstractController
             'lon' => $lon,
             'lat' => $lat,
         ]);
+    }
+
+    #[Route('api/mapboxtoken', name: 'api_mapbox_token', options:['expose' => true])]
+    public function apiMapboxToken(): Response
+    {
+        return new JsonResponse($this->mapbox_token);
     }
 }
